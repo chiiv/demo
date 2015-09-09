44771a1a06c3ac9dd5c59f033391e032ecdd4cbc
Makis Tracend <makis.tracend@gmail.com>
2015-09-09 02:09:19 -0700

    updating state

--- assets/js/app/views/wheel.js
+++ assets/js/app/views/wheel.js
@@ -37,13 +37,28 @@ define(["app/views/common"], function( Common ){
 				'transform': "rotate3d(0,0,1,"+ degrees +"deg)"
 			});
 			// update state
+			this.state.spinning = true;
 			this.state.degrees = degrees;
 			// post transition events
 			$spinner.on('transitionend webkitTransitionEnd oTransitionEnd', _.bind(this.reset, this));
 		},
 
 		reset: function(){
-			console.log("reset");
+			_.log("reset");
+			// release the wheel
+			this.state.spinning = false;
+			// reset degrees to < 360
+			var degrees = this.state.degrees - ( Math.floor( this.state.degrees / 360 ) * 360 );
+			// update dom
+			$(this.el).find(".spinner").removeClass("transition").delay(100).css({
+				'-webkit-transform': "rotate3d(0,0,1,"+ degrees +"deg)",
+				'-o-transform': "rotate3d(0,0,1,"+ degrees +"deg)",
+				'-ms-transform': "rotate3d(0,0,1,"+ degrees +"deg)",
+				'-moz-transform': "rotate3d(0,0,1,"+ degrees +"deg)",
+				'transform': "rotate3d(0,0,1,"+ degrees +"deg)"
+			}).delay(100).addClass("transition");
+			// save new degrees in state
+			this.state.degrees = degrees;
 		}
 
 	});
