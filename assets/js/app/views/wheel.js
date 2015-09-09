468f6cce018e342e3d1464b54c81eacfb94150fa
Makis Tracend <makis.tracend@gmail.com>
2015-09-09 00:47:39 -0700

    disable data-rotate attr

--- assets/js/app/views/wheel.js
+++ assets/js/app/views/wheel.js
@@ -23,7 +23,8 @@ define(["app/views/common"], function( Common ){
 
 			// randomize strength ( 5 < x < 10 )
 			var rotations = 5 + Math.floor( Math.random()* 5 );
-
+			// in the future: update data-rotate attribute
+			//$(this.el).attr("data-rotate", rotations * 360);
 
 		}
 
