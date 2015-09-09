6ea1db401155c173d28a9c074b9e1cdf26d5a087
Makis Tracend <makis.tracend@gmail.com>
2015-09-09 01:35:36 -0700

    rotate spinner

--- assets/js/app/views/wheel.js
+++ assets/js/app/views/wheel.js
@@ -24,8 +24,17 @@ define(["app/views/common"], function( Common ){
 			// randomize strength ( 5 < x < 10 )
 			var rotations = 5 + Math.floor( Math.random()* 5 );
 			// in the future: update data-rotate attribute
-			//$(this.el).attr("data-rotate", rotations * 360);
+			//$(this.el).find(".spinner").attr("data-rotate", rotations * 360);
 
+			$(this.el).find(".spinner").css(
+				{
+					'-webkit-transform': "rotate3d(0,0,1,"+ rotations*360 +"deg)",
+					'-o-transform': "rotate3d(0,0,1,"+ rotations*360 +"deg)",
+					'-ms-transform': "rotate3d(0,0,1,"+ rotations*360 +"deg)",
+					'-moz-transform': "rotate3d(0,0,1,"+ rotations*360 +"deg)",
+					'transform': "rotate3d(0,0,1,"+ rotations*360 +"deg)"
+				}
+			);
 		}
 
 	});
